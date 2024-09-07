71e0acf5bfbcba765e48f4de09f2b29446fa71f6
Pavan Gupta <pavangpay05@gmail.com>
2024-09-07 20:35:01 +0530

    created fronted folder struture

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/ui/button'
+import './App.css'
+
+
+function App() { 
+ 
+
+  return (
+    <>
+   <Button className="bg-red hover:bg-hoverRed">click me</Button>
+    </>
+  )
+}
+
+export default App
